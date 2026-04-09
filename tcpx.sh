667e36da94fdb0bc0da371e488edc030548127d1
torr9522 <torr9522@users.noreply.github.com>
2026-04-10 01:12:22 +0800

    fix: Debian menu-1 image URL selection excludes linux-libc-dev

--- tcpx.sh
+++ tcpx.sh
@@ -571,7 +571,7 @@ installbbr() {
 			kernel_version=$github_ver
 			detele_kernel_head
 			headurl=$(curl -s 'https://api.github.com/repos/torr9522/Linux-NetSpeed/releases' | grep "${github_tag}" | grep 'deb' | grep 'headers' | awk -F '"' '{print $4}')
-			imgurl=$(curl -s 'https://api.github.com/repos/torr9522/Linux-NetSpeed/releases' | grep "${github_tag}" | grep 'deb' | grep -v 'headers' | grep -v 'devel' | awk -F '"' '{print $4}')
+			imgurl=$(curl -s 'https://api.github.com/repos/torr9522/Linux-NetSpeed/releases' | grep "${github_tag}" | grep 'deb' | grep 'image' | grep -v 'headers' | grep -v 'devel' | head -n 1 | awk -F '"' '{print $4}')
 
 			headurl=$(check_cn "$headurl")
 			imgurl=$(check_cn "$imgurl")
@@ -588,7 +588,7 @@ installbbr() {
 			kernel_version=$github_ver
 			detele_kernel_head
 			headurl=$(curl -s 'https://api.github.com/repos/torr9522/Linux-NetSpeed/releases' | grep "${github_tag}" | grep 'deb' | grep 'headers' | awk -F '"' '{print $4}')
-			imgurl=$(curl -s 'https://api.github.com/repos/torr9522/Linux-NetSpeed/releases' | grep "${github_tag}" | grep 'deb' | grep -v 'headers' | grep -v 'devel' | awk -F '"' '{print $4}')
+			imgurl=$(curl -s 'https://api.github.com/repos/torr9522/Linux-NetSpeed/releases' | grep "${github_tag}" | grep 'deb' | grep 'image' | grep -v 'headers' | grep -v 'devel' | head -n 1 | awk -F '"' '{print $4}')
 
 			check_empty "$imgurl"
 			headurl=$(check_cn "$headurl")
